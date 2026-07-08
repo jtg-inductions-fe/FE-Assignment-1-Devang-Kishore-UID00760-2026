3a95b969ed6d3d96296670224c0640483d1b900e
devang kishore <devang.kishore@joshtechnologygroup.com>
2026-07-08 18:24:33 +0530

    [DK_A1_01]: Navbar Fixes Done

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,8 @@
-import { toggle } from './navbar';
-toggle();
+import { toggle, events } from './navbar';
+
+const navbar = document.querySelector('.header');
+const navbarToggle = document.querySelector('.button--sm');
+const navbarContent = document.querySelector('.navbar__content');
+
+toggle(navbarToggle, navbarContent);
+events(navbarContent, navbarToggle, navbar);
--- src/script/navbar.js
+++ src/script/navbar.js
@@ -1,12 +1,9 @@
-const navbarToggle = document.querySelector('.navbar__toggle');
-const navbarContent = document.querySelector('.navbar__content');
 const desktopBreakPoint = 1025;
-const navbar = document.querySelector('.header');
 
 /**
  * Toggle mobile navigation
  */
-const toggle = () => {
+const toggle = (navbarToggle, navbarContent) => {
     if (!navbarToggle || !navbarContent) return;
     navbarToggle.addEventListener('click', () => {
         const isOpen = navbarContent.classList.toggle('navbar__content--open');
@@ -15,28 +12,31 @@ const toggle = () => {
     });
 };
 
-/**
- * Content--open class remove on resize
- */
-window.addEventListener('resize', () => {
-    if (
-        window.innerWidth >= desktopBreakPoint &&
-        navbarContent.classList.contains('navbar__content--open')
-    ) {
-        navbarContent.classList.remove('navbar__content--open');
-        navbarToggle.classList.remove('navbar__toggle--active');
-        navbarToggle.setAttribute('aria-expanded', 'false');
-    }
-});
+const events = (navbarContent, navbarToggle, navbar) => {
+    /**
+     * Content--open class remove on resize
+     */
+    window.addEventListener('resize', () => {
+        if (
+            window.innerWidth >= desktopBreakPoint &&
+            navbarContent.classList.contains('navbar__content--open')
+        ) {
+            navbarContent.classList.remove('navbar__content--open');
+            navbarToggle.classList.remove('navbar__toggle--active');
+            navbarToggle.setAttribute('aria-expanded', 'false');
+        }
+    });
 
-/**
- * Add header--scrolled
- */
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
-        navbar.classList.add('header--scrolled');
-    } else {
-        navbar.classList.remove('header--scrolled');
-    }
-});
-export { toggle };
+    /**
+     * Add header--scrolled
+     */
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 10) {
+            navbar.classList.add('header--scrolled');
+        } else {
+            navbar.classList.remove('header--scrolled');
+        }
+    });
+};
+
+export { toggle, events };
