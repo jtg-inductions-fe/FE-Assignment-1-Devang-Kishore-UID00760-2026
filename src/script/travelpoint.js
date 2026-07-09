106d5252694421829b02fe83cac98b7b97997a9a
devang kishore <devang.kishore@joshtechnologygroup.com>
2026-07-09 13:12:54 +0530

    [DK_A1_03]: Completed Travel point section

--- src/script/travelpoint.js
+++ src/script/travelpoint.js
@@ -1,14 +1,16 @@
 /**
  * Render the cards of travel point
  */
-const renderCards=(stats,statsContainer)=>{
-    let data="";
-    for(let i of stats){
-        data=data+`<div class="stat-card">
+const renderCards = (stats, statsContainer) => {
+    let data = '';
+    for (let i of stats) {
+        data =
+            data +
+            `<div class="stat-card">
                         <h3 class="stat-card__number">${i.number}</h3>
                         <p class="stat-card__label">${i.label}</p>
-                    </div>\n`
+                    </div>\n`;
     }
-    statsContainer.innerHTML=data;
-}
-export {renderCards};
+    statsContainer.innerHTML = data;
+};
+export { renderCards };
