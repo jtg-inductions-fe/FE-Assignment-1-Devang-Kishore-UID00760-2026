fed1b7026bd54521e3dea07698b9879ac5b1cd20
devang kishore <devang.kishore@joshtechnologygroup.com>
2026-07-11 23:22:23 +0530

    [DK_A1_05]: Complete footer section

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
 import data from '../data/content.json';
 import { toggle, setOpen, setScrolled } from './navbar';
 import renderCards from './travelpoint';
+import toggleAccordion from './accordion';
 import renderTestimonials from './carousel';
+
 /**
  * QuerySelectors for navbar elements
  */
@@ -12,15 +14,24 @@ const statsContainer = document.querySelector('.travel-point__stats');
 const DESKTOP_BREAK_POINT = 1024;
 const Y_SCROLL = 10;
 const stats = data['travel-point'].stats;
+const footerContainer = document.querySelector('#footer-lists');
 
 /**
  * Function calls
  */
 renderCards(stats, statsContainer);
 renderTestimonials(data.testimonials);
+
 /**
  * Event Listeners
  */
+footerContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('footer__heading')) {
+        const targetList=event.target.nextElementSibling;
+        toggleAccordion(event.target,targetList);
+    }
+});
+
 window.addEventListener('resize', () => {
     setOpen(navbarLinksMenu, navbarHamburgerButton, DESKTOP_BREAK_POINT);
 });
