e3f3cdb1d942e63541ba2348428e9c0e3020a25d
devang kishore <devang.kishore@joshtechnologygroup.com>
2026-07-11 17:01:26 +0530

    [DK_A1_03]: Travel point fixes done

--- src/script/travelpoint.js
+++ src/script/travelpoint.js
@@ -1,18 +1,36 @@
 /**
+ * Compacts the number into shorthand form.
+ * @param {number} statNumber - number which needs to be compacted
+ */
+const compactNumber = (statNumber) => {
+    const formatter = new Intl.NumberFormat('en', { notation: 'compact' });
+    if (statNumber < 100) {
+        return statNumber;
+    } else if (statNumber < 1000) {
+        return statNumber - (statNumber % 100) == statNumber
+            ? statNumber
+            : `${statNumber - (statNumber % 100)}+`;
+    }
+    return formatter.format(statNumber);
+};
+
+/**
  * Render the cards of travel point
  * @param {Object} stats - data of stats
  * @param {string} statsContainer - container in which stats need to rendered.
  */
 const renderCards = (stats, statsContainer) => {
-    let data = '';
-    for (let i of stats) {
-        data =
-            data +
-            `<div class="stat-card">
-                        <h3 class="stat-card__number">${i.number}</h3>
-                        <p class="stat-card__label">${i.label}</p>
-                    </div>\n`;
-    }
-    statsContainer.innerHTML = data;
+    stats.forEach((statObject) => {
+        let container = document.createElement('div');
+        container.className = 'stat-card';
+        let heading = document.createElement('h3');
+        heading.textContent = `${compactNumber(statObject.number)}`;
+        heading.className = 'stat-card__number';
+        let paragraph = document.createElement('p');
+        paragraph.textContent = `${statObject.label}`;
+        paragraph.className = 'stat-card__label';
+        container.append(heading, paragraph);
+        statsContainer.append(container);
+    });
 };
 export default renderCards;
